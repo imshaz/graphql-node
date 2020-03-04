3f64097f1262e657b14fa9fa415c917cd5abe3ab
imshaz <imshazbangash@gmail.com>
2020-03-04 13:12:08 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const {GraphQLServer} ='graphql-yoga';
+
+// 1 
+const typeDefs =`
+type Query{
+    info:String!
+}`
+
+
+// 2 
+
+const resolvers ={
+Query:{
+    info:()=.'This is API of a hacker-news clone'
+}
+}
+
+// 3
+
+const server = new GraphQLServer({
+    typeDefs, 
+    resolvers
+})
+
+server.start(()=>console.log('Server is running on http:localhost:4000'))

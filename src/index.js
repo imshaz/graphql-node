e2af5796104565f0417037266941493d9a0385e1
imshaz <imshazbangash@gmail.com>
2020-03-05 12:26:30 -0500

    added single link query

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 // import {GraphQLServer} from 'graphql-yoga';
 
 const { GraphQLServer } = require('graphql-yoga')
-
+const { v4: uuid } = require('uuid');
 let links = [{
-  id: 'link-0',
+  id: '9a3bce97-ff86-4a2c-823e-aabbd28e853a',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
 
-let idCount = links.length
+// let idCount = links.length
 // 2
 const resolvers = {
   Query: {
     info: () => `this is Graphql API`,
-    feed:()=>links
+    feed:()=>links,
+    link: (parent, args, context, info)=>{
+      const {id} =args
+        let index = links.findIndex(item=>{
+            return item.id===id
+          }
+        )
+
+
+        if(index===-1) return null
+
+       let item= links.splice(index, 1)
+
+        return item[0]
+    }
   },
   Mutation:{
 
     post:(parent, args)=>{
       const link = {
-        id: `link-${idCount++}`,
+        id: uuid(),
         description:args.description, 
         url:args.url 
       }
